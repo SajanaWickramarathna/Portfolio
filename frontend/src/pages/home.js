08d85efade16e7d77387d47e983861e1977583b7
Sajana Wickramarathna <sajanawickramarathna143@gmail.com>
2025-10-08 21:30:56 +0530

    CHANGE IMAGE

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -10,7 +10,7 @@ import {
 import { FaXTwitter } from "react-icons/fa6";
 import Footer from "../components/footer";
 
-import saj from "../assets/saj.jpg";
+import saj from "../assets/saj.JPG";
 import { useNavigate } from "react-router-dom";
 
 const socialLinks = [
